8b5c744d6b02e5b903222d4de68f5b920cfcf40c
Francisco Vasconcelos Junior <francisco.vasconcelos@funceme.br>
2024-03-08 09:49:35 -0300

    sfc plev

--- down_era5_monthly_means_plev.py
+++ down_era5_monthly_means_plev.py
@@ -16,34 +16,34 @@ def get_era(var=str, pres_levels=list, year=int, months=list):
     #day=int(date[6:8])
 
     c.retrieve(
-    'reanalysis-era5-single-levels-monthly-means',
+    'reanalysis-era5-pressure-levels-monthly-means',
          {
         'product_type': 'monthly_averaged_reanalysis',
         'variable': var,
-#        'pressure_level': [
-#             '10', ' 50', '70',
-#            '100', '125', '150',
-#            '175', '200', '225',
-#            '250', '300', '350',
-#            '400', '450', '500',
-#            '550', '600', '650',
-#            '700', '750', '775',
-#            '800', '825', '850',
-#            '875', '900', '925',
-#            '950', '975','1000',
-#        ],
+        'pressure_level': [
+            '100', '125', '150',
+            '175', '200', '225',
+            '250', '300', '350',
+            '400', '450', '500',
+            '550', '600', '650',
+            '700', '750', '775',
+            '800', '825', '850',
+            '875', '900', '925',
+            '950', '975','1000',
+        ],
         'year': str(year),
         'month':
             months
         ,
         'time': '00:00',
+        'grid' : [0.5, 0.5],
 #        'area': [
 #            90, -180, -90,
 #                 180,
 #        ],
         'format': 'netcdf',
     },
-    f'era5_sst_monthly_sep-oct-nov_{year}.nc')
+    f'era5_omega_monthly_{year}_0p5.nc')
 
 year=args.year
 
@@ -51,8 +51,8 @@ print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
 for YY in range(year,year+1):
     print("Download year: ", YY,'\n')
-
-    get_era('sea_surface_temperature', None, year=YY, months=['09','10','11'])
+    months = [f"{ff:02d}" for ff in range(1,13)]
+    get_era('vertical_velocity', None, year=YY, months=months)
     print()
 
 print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
--- down_era5_monthly_means_sfc.py
+++ down_era5_monthly_means_sfc.py
@@ -20,21 +20,8 @@ def get_era(var=str, pres_levels=list, year=int, months=list):
          {
         'product_type': 'monthly_averaged_reanalysis',
         'variable': var,
-#        'pressure_level': [
-#             '10', ' 50', '70',
-#            '100', '125', '150',
-#            '175', '200', '225',
-#            '250', '300', '350',
-#            '400', '450', '500',
-#            '550', '600', '650',
-#            '700', '750', '775',
-#            '800', '825', '850',
-#            '875', '900', '925',
-#            '950', '975','1000',
-#        ],
         'year': str(year),
-        'month':
-            months
+        'month': months
         ,
         'time': '00:00',
         'grid': [0.5, 0.5],
